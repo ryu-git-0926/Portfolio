a007cc9329abaa6eae3022670481ad7c88426362
ryu-git-0926 <ryu091026@outlook.jp>
2021-05-27 16:48:55 +0900

    deviseでのログイン, 新規登録のフォーム完了

--- Gemfile
+++ Gemfile
@@ -54,3 +54,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'bootstrap'
+gem 'jquery-rails'
+gem 'devise'
+gem 'devise-i18n'
+gem 'devise-i18n-views'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'home#top'
+  get '/posts',  to: 'home#posts'
+  devise_scope :user do
+    get '/users/sign_out',  to: 'devise/sessions#destroy'
+  end
 end
